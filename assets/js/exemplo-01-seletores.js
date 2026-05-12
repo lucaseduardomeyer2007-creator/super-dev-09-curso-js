13ca4a6ecd6ce190fb0795488e83f27198b5bec4
lucaseduardomeyer2007-creator <lucaseduardomeyer2007@gmail.com>
2026-05-11 21:51:03 -0300

    Curso

---
+++ assets/js/exemplo-01-seletores.js
@@ -0,0 +1,91 @@
+function concatenar() {
+    //get element by => pegar elemento por id
+    const campoNome = document.getElementById("nome");
+    const campoSobrenome = document.getElementById("sobrenome")
+    const nome = campoNome.value;
+    const sobrenome = campoSobrenome.value;
+
+    alert(`Nome Completo: ${nome} ${sobrenome}`);
+
+}
+
+function verificarPar() {
+
+    const campoNumero = document.getElementById("numero1");
+    const numero1 = parseInt(campoNumero.value);
+
+
+    if (numero1 % 2 === 0) {
+        alert("Número é par")
+    }
+    else {
+        alert("Número é ímpar")
+    }
+
+    // Limpar campo
+    campoNumero.value = "";
+    // Colocar o cursor no campo
+    campoNumero.focus()
+}
+
+function processarPedido() {
+    const campoCliente = document.getElementById("cliente")
+    const cliente = campoCliente.value;
+
+    const campoQuantidadeLicencas = document.getElementById("quantidade-licencas");
+    const quantidadeLicencas = parseInt(campoQuantidadeLicencas.value);
+
+    const campoTipo = document.querySelector("[name='tipo']:checked");
+    if (campoTipo === null) {
+        alert("Selecione um tipo")
+        return;
+    }
+    const tipo = campoTipo.value;
+
+    const campoPlano = document.getElementById("plano")
+    const plano = campoPlano.value;
+
+    if ((plano !== "Básico") && (plano !== "Intermediário") && (plano !== "Avançado")) {
+        alert("Escolha um plano")
+        return;
+    }
+
+    const campoDataEncerramento = document.getElementById("data-encerramento-contrato");
+    const dataEncerramento = campoDataEncerramento.value;
+
+    const campoBackup = document.getElementById("backup");
+    const backup = campoBackup.checked;
+
+    let precoBase = 0;
+if(tipo === "ERP") {
+    precoBase = 250;
+} else if(tipo === "Hospital") {
+    precoBase = 400;
+}
+else if(tipo === "Oficina") {
+    precoBase = 100;
+}
+
+let multiplicadorPlano = 1.0;
+    if(plano === "Intermediário") {
+        multiplicadorPlano = 1.5
+    }
+    else if(plano === "Avançado") {
+        multiplicadorPlano = 2.0;
+    }
+
+    let total = quantidadeLicencas * precoBase * multiplicadorPlano;
+
+    const campoResultado = document.getElementById("resultado");
+    campoResultado.value = `
+    
+    Cliente: ${cliente}
+    Quantidade de liçenças: ${quantidadeLicencas}
+    Tipo: ${tipo}
+    Plano: ${plano}
+    Data de Encerramento: ${dataEncerramento}
+    Backup: ${backup ? "Sim" : "Não"}
+    Preço Base: ${precoBase.toFixed(2)}
+    Multiplicador do Plano: ${multiplicadorPlano}
+    Total: R$: ${total.toFixed(2)}`
+}
---
+++ assets/js/exercicio-01-seletores.js
@@ -0,0 +1,10 @@
+function exercicio01CompararDoisNumeros() {
+const campoNumero1 = document.getElementById("numero1")
+const numero1 = parseInt(campoNumero1.value);
+
+const campoNumero2 = document.getElementById("numero2")
+const numero2 = parseInt(campoNumero2.value);
+
+alert(`Número 1: ${numero1}
+    Número 2: ${numero2}`)
+}
