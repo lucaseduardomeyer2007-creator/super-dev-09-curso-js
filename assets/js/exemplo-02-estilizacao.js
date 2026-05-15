6d9bfa266b4bb0cfe7b9a1b08ca60986ec5a9364
lucaseduardomeyer2007-creator <lucaseduardomeyer2007@gmail.com>
2026-05-15 17:46:27 -0300

    Curso

--- assets/js/exemplo-02-estilizacao.js
+++ assets/js/exemplo-02-estilizacao.js
@@ -70,10 +70,22 @@ function apresentarCamposPessoaJuridica() {
     divCamposPf.classList.add("hidden");
 }
 
-function alterarImagem(evento) {
-    if(evento.key === "enter") {
+const imagemDragonBall = "https://ovicio.com.br/wp-content/uploads/dragon-ball-z-vegeta-9000-238679.png";
+const imagemAbsoluteCinema = "https://lazersemfronteiras.com.br/wp-content/uploads/2025/09/Absolute-cinema-o-que-e-e-por-que-esse-meme-viralizou.jpg";
+const imagemSapo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwqd6Yl7zik3g7UBl3_gM_OYK3IKpGL2fJeQ&s";
 
 
-        
+function alterarImagem(evento){
+    if(evento.key === "Enter"){
+        const imagem = document.getElementsByClassName("imagem")[0];
+        const campoPesquisa = document.getElementById("campo-pesquisa");
+        const pesquisa = campoPesquisa.value;
+        if(pesquisa === "dragon ball"){
+            imagem.style.background = `url('${imagemDragonBall}')`;
+        }else if(pesquisa === "absolute cinema"){
+            imagem.style.background = `url('${imagemAbsoluteCinema}')`;
+        }else{
+            imagem.style.background = `url('${imagemSapo}')`;
+        }
     }
 }
