81eccfb5fd89c7e27b7c3ecd51817c3f8b884e54
Lucas Eduardo <lucaseduardomeyer2007@gmail.com>
2026-05-24 15:19:42 -0300

    curso

---
+++ assets/js/exercicio-04-criar-elementos.js
@@ -0,0 +1,15 @@
+function CriarMuralRecado() {
+
+const campoNome = document.getElementById("nome-mural");
+const nome = campoNome.value;
+
+
+const campoRecado = document.getElementById("recado");
+const recado = campoRecado.value;
+
+const horarioRecadoPublicado = new Date();
+
+
+
+
+}
